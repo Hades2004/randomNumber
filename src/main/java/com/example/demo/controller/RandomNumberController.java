667483c1ef31ebf4e739f177bbfd6bb4d922368c
root <root@erik-MS-7676>
2023-06-03 11:57:34 +0200

    eb: remove last / in url

--- src/main/java/com/example/demo/controller/RandomNumberController.java
+++ src/main/java/com/example/demo/controller/RandomNumberController.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/randomNumber")
 public class RandomNumberController {
 	
-	@GetMapping("/")
+	@GetMapping("")
 	private Mono<Integer> getEmployeeById() {
 	    return Mono.just(5);
 	}
