540b3ea2d6d833ff2bbd718f4f5809ccd025f84a
Hades2004 <erikbong92@gmail.com>
2026-02-26 15:04:15 +0700

    add auth to healthchecks

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO CUSTOM_USER (username, password)
-VALUES ('user','$2a$10$ZkZA2Dl3.AhzgEiG.grCPeE1pGstfhNARSFcs8oImpFABp5sNhC5e');
+INSERT INTO CUSTOM_USER (username, password) VALUES ('user','$2a$10$ZkZA2Dl3.AhzgEiG.grCPeE1pGstfhNARSFcs8oImpFABp5sNhC5e');
+INSERT INTO CUSTOM_USER (username, password) VALUES ('monitoring','$2a$10$ZkZA2Dl3.AhzgEiG.grCPeE1pGstfhNARSFcs8oImpFABp5sNhC5e');
 
 INSERT INTO ROLE (id, name) VALUES (1, 'ROLE_USER');
 INSERT INTO ROLE (id, name) VALUES (2, 'ROLE_ADMIN');
 INSERT INTO ROLE (id, name) VALUES (3, 'ROLE_MONITORING');
 
-INSERT INTO APP_USER_ROLES (app_user_id, role_id)
-VALUES (1, 1);
+INSERT INTO APP_USER_ROLES (app_user_id, role_id) VALUES (1, 1);
+INSERT INTO APP_USER_ROLES (app_user_id, role_id) VALUES (2, 3);
